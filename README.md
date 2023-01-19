e04b300c5fbeda9d4e88834b0159eed00cd1d5fc
Sardonyx <canboraciner@gmail.com>
2023-01-20 01:15:14 +0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,17 @@
+<p align="center">
+  <img width="256" height="256" src="https://raw.githubusercontent.com/cborac/twitter-views-on-the-left/main/assets/logo256.png">
+</p>
+
+# Twitter Views on the Left
+
+Accidentally clicking the views button instead of the like button when scrolling through your Home page is very annoying. At least I was very annoyed. So I wrote this userscript to fix that.
+
+:-------------------------:|:-------------------------:
+Before  |  After
+![](https://raw.githubusercontent.com/cborac/twitter-views-on-the-left/main/assets/before.png) | ![](https://raw.githubusercontent.com/cborac/twitter-views-on-the-left/main/assets/after.png)
+
+# Installation
+
+* Make sure you have user scripts enabled in your browser. *([TemperMonkey](https://www.tampermonkey.net/) is a good extension for such purpose)*
+* After installing the proper extension to your browser [Click Here](https://raw.githubusercontent.com/cborac/twitter-views-on-the-left/main/moveToLeft.user.js)
+* Done!
---
+++ moveToLeft.user.js
@@ -0,0 +1,35 @@
+// ==UserScript==
+// @name         Twitter Views on the Left
+// @namespace    https://boraciner.com/
+// @version      1.0
+// @description  Moves the Twitter views to the left side of the screen
+// @author       Bora Ciner
+// @match        https://twitter.com/*
+// @iconURL      https://raw.githubusercontent.com/cborac/twitter-views-on-the-left/main/assets/logo256.png
+// @grant        none
+// @updateURL    https://raw.githubusercontent.com/cborac/twitter-views-on-the-left/main/moveToLeft.user.js
+// @downloadURL  https://raw.githubusercontent.com/cborac/twitter-views-on-the-left/main/moveToLeft.user.js
+// ==/UserScript==
+
+
+const observer = new MutationObserver((records) => {
+     const nodes = records.flatMap(x => Array.from(x.addedNodes), 2)
+     if (nodes.length == 0) return
+
+     const btns = Array.from(document.querySelectorAll('[aria-label$="View Tweet analytics"]')).filter(x => !x.getAttribute("modified"))
+
+     btns.forEach(x => {
+          const el = x.parentElement
+          const container = x.parentElement.parentElement
+
+          container.childNodes[0].style.marginLeft = "100px";
+          el.style.position = "absolute";
+
+          x.setAttribute("modified", true)
+     })
+})
+
+observer.observe(document, {
+     childList: true,
+     subtree: true
+})
